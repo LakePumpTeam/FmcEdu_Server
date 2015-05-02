a3196dab0950b324bd85836559abbbcea0795a69
Yove <billlllyove@hotmail.com>
2015-05-03 02:41:19 +0800

    Commit at 2015-05-03 02:40
    Finished ddl sql and removed test change set

---
+++ src/main/resources/liquibase/ddl.sql
@@ -0,0 +1,186 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0;
+SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0;
+SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema fmc_edu
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `fmc_edu`;
+
+-- -----------------------------------------------------
+-- Schema fmc_edu
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `fmc_edu`
+  DEFAULT CHARACTER SET utf8
+  COLLATE utf8_general_ci;
+USE `fmc_edu`;
+
+-- -----------------------------------------------------
+-- Table `fmc_edu`.`Province`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `fmc_edu`.`Province`;
+
+CREATE TABLE IF NOT EXISTS `fmc_edu`.`Province` (
+  `Id`   INT         NOT NULL AUTO_INCREMENT,
+  `Name` VARCHAR(40) NOT NULL,
+  PRIMARY KEY (`Id`)
+)
+  ENGINE = MyISAM;
+
+
+-- -----------------------------------------------------
+-- Table `fmc_edu`.`City`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `fmc_edu`.`City`;
+
+CREATE TABLE IF NOT EXISTS `fmc_edu`.`City` (
+  `Id`          INT         NOT NULL AUTO_INCREMENT,
+  `Name`        VARCHAR(40) NOT NULL,
+  `Province_Id` INT         NOT NULL,
+  PRIMARY KEY (`Id`)
+)
+  ENGINE = MyISAM;
+
+CREATE INDEX `fk_City_Province_idx` ON `fmc_edu`.`City` (`Province_Id` ASC);
+
+
+-- -----------------------------------------------------
+-- Table `fmc_edu`.`Address`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `fmc_edu`.`Address`;
+
+CREATE TABLE IF NOT EXISTS `fmc_edu`.`Address` (
+  `Id`           INT          NOT NULL AUTO_INCREMENT,
+  `Full_Address` VARCHAR(200) NULL,
+  `Province_Id`  INT          NOT NULL,
+  `City_Id`      INT          NOT NULL,
+  PRIMARY KEY (`Id`)
+)
+  ENGINE = MyISAM;
+
+CREATE INDEX `fk_Address_Province1_idx` ON `fmc_edu`.`Address` (`Province_Id` ASC);
+
+CREATE INDEX `fk_Address_City1_idx` ON `fmc_edu`.`Address` (`City_Id` ASC);
+
+
+-- -----------------------------------------------------
+-- Table `fmc_edu`.`School`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `fmc_edu`.`School`;
+
+CREATE TABLE IF NOT EXISTS `fmc_edu`.`School` (
+  `Id`          INT         NOT NULL AUTO_INCREMENT,
+  `Name`        VARCHAR(80) NULL,
+  `Province_Id` INT         NOT NULL,
+  `City_Id`     INT         NOT NULL,
+  `Address_Id`  INT         NULL,
+  PRIMARY KEY (`Id`)
+)
+  ENGINE = MyISAM;
+
+CREATE INDEX `fk_School_City1_idx` ON `fmc_edu`.`School` (`City_Id` ASC);
+
+CREATE INDEX `fk_School_Address1_idx` ON `fmc_edu`.`School` (`Address_Id` ASC);
+
+CREATE INDEX `fk_School_Province1_idx` ON `fmc_edu`.`School` (`Province_Id` ASC);
+
+
+-- -----------------------------------------------------
+-- Table `fmc_edu`.`Teacher`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `fmc_edu`.`Teacher`;
+
+CREATE TABLE IF NOT EXISTS `fmc_edu`.`Teacher` (
+  `Id`           INT         NOT NULL AUTO_INCREMENT,
+  `Name`         VARCHAR(20) NOT NULL,
+  `Phone`        VARCHAR(11) NOT NULL,
+  `Password`     VARCHAR(32) NOT NULL,
+  `Class_Id`     INT         NULL,
+  `School_Id`    INT         NOT NULL,
+  `Sub_Title`    VARCHAR(20) NOT NULL,
+  `Head_Teacher` TINYINT(1)  NOT NULL,
+  `App_Id`       VARCHAR(20) NULL,
+  `Available`    TINYINT(1)  NULL     DEFAULT 1,
+  PRIMARY KEY (`Id`)
+)
+  ENGINE = MyISAM;
+
+CREATE INDEX `fk_Teacher_Class1_idx` ON `fmc_edu`.`Teacher` (`Class_Id` ASC);
+
+CREATE INDEX `fk_Teacher_School1_idx` ON `fmc_edu`.`Teacher` (`School_Id` ASC);
+
+
+-- -----------------------------------------------------
+-- Table `fmc_edu`.`Class`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `fmc_edu`.`Class`;
+
+CREATE TABLE IF NOT EXISTS `fmc_edu`.`Class` (
+  `Id`              INT         NOT NULL AUTO_INCREMENT,
+  `School_id`       INT         NOT NULL,
+  `Grade`           VARCHAR(50) NOT NULL,
+  `Class`           VARCHAR(50) NOT NULL,
+  `Available`       TINYINT(1)  NOT NULL DEFAULT 1,
+  `Head_Teacher_Id` INT         NULL,
+  PRIMARY KEY (`Id`)
+)
+  ENGINE = MyISAM;
+
+CREATE INDEX `fk_Class_School1_idx` ON `fmc_edu`.`Class` (`School_id` ASC);
+
+CREATE INDEX `fk_Class_Teacher1_idx` ON `fmc_edu`.`Class` (`Head_Teacher_Id` ASC);
+
+
+-- -----------------------------------------------------
+-- Table `fmc_edu`.`Student`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `fmc_edu`.`Student`;
+
+CREATE TABLE IF NOT EXISTS `fmc_edu`.`Student` (
+  `Id`            INT         NOT NULL AUTO_INCREMENT,
+  `Name`          VARCHAR(20) NOT NULL,
+  `Class_id`      INT         NOT NULL,
+  `Sex`           TINYINT(1)  NULL,
+  `Birth`         DATE        NULL,
+  `Ring_Number`   VARCHAR(45) NULL,
+  `Creation_Date` DATETIME    NULL,
+  `Available`     TINYINT(1)  NULL     DEFAULT 1,
+  PRIMARY KEY (`Id`)
+)
+  ENGINE = MyISAM;
+
+CREATE INDEX `fk_Student_Class1_idx` ON `fmc_edu`.`Student` (`Class_id` ASC);
+
+
+-- -----------------------------------------------------
+-- Table `fmc_edu`.`Parent`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `fmc_edu`.`Parent`;
+
+CREATE TABLE IF NOT EXISTS `fmc_edu`.`Parent` (
+  `Id`              INT         NOT NULL AUTO_INCREMENT,
+  `Name`            VARCHAR(20) NOT NULL,
+  `Phone`           VARCHAR(11) NOT NULL,
+  `Password`        VARCHAR(32) NOT NULL,
+  `Student_Id`      INT         NOT NULL,
+  `Address_Id`      INT         NULL,
+  `Approved`        TINYINT(1)  NULL     DEFAULT 0,
+  `Approved_Date`   DATETIME    NULL,
+  `App_Id`          VARCHAR(19) NULL,
+  `Creation_Date`   DATETIME    NULL,
+  `Available`       TINYINT(1)  NULL     DEFAULT 1,
+  `Last_Login_Date` DATETIME    NULL,
+  PRIMARY KEY (`Id`)
+)
+  ENGINE = MyISAM;
+
+CREATE INDEX `fk_Parent_Student1_idx` ON `fmc_edu`.`Parent` (`Student_Id` ASC);
+
+CREATE INDEX `fk_Parent_Address1_idx` ON `fmc_edu`.`Parent` (`Address_Id` ASC);
+
+
+SET SQL_MODE = @OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS;
