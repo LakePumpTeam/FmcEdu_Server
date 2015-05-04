ec21e5977eb8a4bdb2f5d3a4f91e8a54f9ce369a
helloyuwei <753036576@qq.com>
2015-05-04 23:31:41 +0800

    Commit at 2015-05-03 02:40
    Reflect BaseController and add base64 encode for output message.

--- src/main/java/com/fmc/edu/constant/GlobalConstant.java
+++ src/main/java/com/fmc/edu/constant/GlobalConstant.java
@@ -7,9 +7,19 @@ import org.apache.commons.lang3.StringUtils;
  */
 public interface GlobalConstant {
 
-	String URL_SUFFIX = StringUtils.EMPTY;
+    String URL_SUFFIX = StringUtils.EMPTY;
 
-	String CONFIG_DOMAIN = "domain";
+    String CONFIG_DOMAIN = "domain";
 
-	String CONFIG_CONTEXT = "context";
+    String CONFIG_CONTEXT = "context";
+
+    String CHARSET_UTF8 = "utf-8";
+
+    boolean SUCCESS = true;
+
+    boolean FAILED = false;
+
+    int STATUS_SUCCESS = 0;
+
+    int STATUS_ERROR = -1;
 }
--- src/main/java/com/fmc/edu/web/controller/BaseController.java
+++ src/main/java/com/fmc/edu/web/controller/BaseController.java
@@ -1,9 +1,12 @@
 package com.fmc.edu.web.controller;
 
+import com.fmc.edu.constant.GlobalConstant;
 import com.fmc.edu.constant.JSONOutputConstant;
 import net.sf.json.JSONObject;
 import org.apache.commons.lang3.StringUtils;
+import sun.misc.BASE64Encoder;
 
+import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,24 +15,26 @@ import java.util.Map;
  */
 public abstract class BaseController {
 
-	protected static final String SUCCESS = "0";
+    protected int getStatusMapping(boolean pSuccess) {
+        return pSuccess ? GlobalConstant.STATUS_SUCCESS : GlobalConstant.STATUS_ERROR;
+    }
 
-	protected static final String ERROR = "-1";
+    protected String generateJsonOutput(boolean pSuccess, Object pJsonData, String pMessage) {
+        Map<String, Object> result = new HashMap<>();
+        result.put(JSONOutputConstant.PARAM_STATUS, getStatusMapping(pSuccess));
+        if (pJsonData == null) {
+            pJsonData = StringUtils.EMPTY;
+        }
+        result.put(JSONOutputConstant.PARAM_DATA, pJsonData);
+        if (pMessage == null) {
+            pMessage = StringUtils.EMPTY;
+        }
+        result.put(JSONOutputConstant.PARAM_MESSAGE, pMessage);
+        return encodeOutput(JSONObject.fromObject(result).toString());
+    }
 
-
-	protected String getStatusMapping(boolean pSuccess) {
-		return pSuccess ? SUCCESS : ERROR;
-	}
-
-	protected String generateJsonOutput(boolean pSuccess, Object pJsonData, String pMessage) {
-		Map<String, Object> result = new HashMap<>();
-		result.put(JSONOutputConstant.PARAM_STATUS, getStatusMapping(pSuccess));
-		if (pJsonData != null) {
-			result.put(JSONOutputConstant.PARAM_DATA, pJsonData);
-		}
-		if (StringUtils.isNoneBlank(pMessage)) {
-			result.put(JSONOutputConstant.PARAM_MESSAGE, pMessage);
-		}
-		return JSONObject.fromObject(result).toString();
-	}
+    protected String encodeOutput(final String pMessage) {
+        if (pMessage == null) return null;
+        return (new BASE64Encoder()).encode(pMessage.getBytes(Charset.forName(GlobalConstant.CHARSET_UTF8)));
+    }
 }
