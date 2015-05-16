e828a2f7edfae9e910bd7084e2e49e0195c2d933
helloyuwei <753036576@qq.com>
2015-05-16 22:58:15 +0800

    Commit at 2015-05-03 02:40

--- src/main/java/com/fmc/edu/model/relationship/ParentStudentRelationship.java
+++ src/main/java/com/fmc/edu/model/relationship/ParentStudentRelationship.java
@@ -7,71 +7,71 @@ import java.sql.Timestamp;
  */
 public class ParentStudentRelationship extends BaseRelationship {
 
-	private int mParentId;
+    private int mParentId;
 
-	private String mParentPhone;
+    private String mParentPhone;
 
-	private int mStudentId;
+    private int mStudentId;
 
-	private String mRelationship;
+    private String mRelationship;
 
-	private boolean mApproved;
+    private int mApproved;
 
-	private Timestamp mApprovedDate;
+    private Timestamp mApprovedDate;
 
-	public ParentStudentRelationship() {
-	}
+    public ParentStudentRelationship() {
+    }
 
-	public ParentStudentRelationship(final String pParentPhone, final String pRelationship) {
-		mParentPhone = pParentPhone;
-		mRelationship = pRelationship;
-	}
+    public ParentStudentRelationship(final String pParentPhone, final String pRelationship) {
+        mParentPhone = pParentPhone;
+        mRelationship = pRelationship;
+    }
 
-	public int getParentId() {
-		return mParentId;
-	}
+    public int getParentId() {
+        return mParentId;
+    }
 
-	public void setParentId(final int pParentId) {
-		mParentId = pParentId;
-	}
+    public void setParentId(final int pParentId) {
+        mParentId = pParentId;
+    }
 
-	public String getParentPhone() {
-		return mParentPhone;
-	}
+    public String getParentPhone() {
+        return mParentPhone;
+    }
 
-	public void setParentPhone(final String pParentPhone) {
-		mParentPhone = pParentPhone;
-	}
+    public void setParentPhone(final String pParentPhone) {
+        mParentPhone = pParentPhone;
+    }
 
-	public int getStudentId() {
-		return mStudentId;
-	}
+    public int getStudentId() {
+        return mStudentId;
+    }
 
-	public void setStudentId(final int pStudentId) {
-		mStudentId = pStudentId;
-	}
+    public void setStudentId(final int pStudentId) {
+        mStudentId = pStudentId;
+    }
 
-	public String getRelationship() {
-		return mRelationship;
-	}
+    public String getRelationship() {
+        return mRelationship;
+    }
 
-	public void setRelationship(final String pRelationship) {
-		mRelationship = pRelationship;
-	}
+    public void setRelationship(final String pRelationship) {
+        mRelationship = pRelationship;
+    }
 
-	public boolean isApproved() {
-		return mApproved;
-	}
+    public int getApproved() {
+        return mApproved;
+    }
 
-	public void setApproved(final boolean pApproved) {
-		mApproved = pApproved;
-	}
+    public void setApproved(int pApproved) {
+        this.mApproved = pApproved;
+    }
 
-	public Timestamp getApprovedDate() {
-		return mApprovedDate;
-	}
+    public Timestamp getApprovedDate() {
+        return mApprovedDate;
+    }
 
-	public void setApprovedDate(final Timestamp pApprovedDate) {
-		mApprovedDate = pApprovedDate;
-	}
+    public void setApprovedDate(final Timestamp pApprovedDate) {
+        mApprovedDate = pApprovedDate;
+    }
 }
