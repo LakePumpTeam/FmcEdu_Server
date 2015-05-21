6551de91e023c9552ec9008053d77836c4b24749
helloyuwei <753036576@qq.com>
2015-05-21 21:11:34 +0800

    Commit at 2015-05-03 02:40 update ddl

--- src/main/resources/liquibase/ddl.sql
+++ src/main/resources/liquibase/ddl.sql
@@ -121,14 +121,17 @@ CREATE TABLE IF NOT EXISTS `fmc_edu`.`profile` (
   `name` VARCHAR(20) NULL,
   `phone` VARCHAR(11) NOT NULL,
   `password` VARCHAR(32) NULL,
+  `salt` VARCHAR(32) NULL,
   `app_id` VARCHAR(20) NULL,
   `channel_id` DECIMAL(20) NULL,
-  `salt` VARCHAR(32) NULL,
   `creation_date` DATETIME NULL,
   `last_login_date` DATETIME NULL,
   `last_update_date` DATETIME NOT NULL,
   `available` TINYINT(1) NOT NULL DEFAULT 1,
   `profile_type` TINYINT(1) NOT NULL DEFAULT 1,
+  `last_pc_id` INT NULL,
+  `last_cl_id` INT NULL,
+  `last_sc_id` INT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `phone_UNIQUE` (`phone` ASC))
 ENGINE = InnoDB;
@@ -211,6 +214,142 @@ CREATE TABLE IF NOT EXISTS `fmc_edu`.`teacher_class_map` (
   `last_update_date` DATETIME NOT NULL,
   PRIMARY KEY (`teacher_id`, `class_id`))
 ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `fmc_edu`.`news`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `fmc_edu`.`news` ;
+
+CREATE TABLE IF NOT EXISTS `fmc_edu`.`news` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `author` INT NULL,
+  `subject` VARCHAR(60) NOT NULL,
+  `content` VARCHAR(4000) NOT NULL,
+  `news_type` TINYINT(1) NOT NULL DEFAULT 0,
+  `ref_id` INT NULL,
+  `approved` TINYINT(1) NULL DEFAULT 0,
+  `approved_by` INT NULL,
+  `approve_date` DATETIME NULL,
+  `like` INT NULL DEFAULT 0,
+  `publish_date` DATETIME NULL,
+  `creation_date` DATETIME NOT NULL,
+  `last_update_date` DATETIME NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `fmc_edu`.`comments`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `fmc_edu`.`comments` ;
+
+CREATE TABLE IF NOT EXISTS `fmc_edu`.`comments` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `news_id` INT NOT NULL,
+  `profile_id` INT NOT NULL,
+  `comment` VARCHAR(500) NOT NULL,
+  `creation_date` DATETIME NULL,
+  `last_update_date` DATETIME NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `fmc_edu`.`user_message`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `fmc_edu`.`user_message` ;
+
+CREATE TABLE IF NOT EXISTS `fmc_edu`.`user_message` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `message_type` TINYINT(2) NOT NULL DEFAULT 0,
+  `content` VARCHAR(400) NOT NULL,
+  `ref_id` INT NOT NULL,
+  `creation_date` DATETIME NULL,
+  `last_update_date` DATETIME NOT NULL,
+  `reading` INT NULL DEFAULT 0,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `fmc_edu`.`timetable`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `fmc_edu`.`timetable` ;
+
+CREATE TABLE IF NOT EXISTS `fmc_edu`.`timetable` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `class_id` INT NOT NULL,
+  `available` TINYINT(1) NULL DEFAULT 1,
+  `creation_date` DATETIME NULL,
+  `last_update_date` DATETIME NULL,
+  PRIMARY KEY (`id`))
+ENGINE = MyISAM;
+
+
+-- -----------------------------------------------------
+-- Table `fmc_edu`.`course`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `fmc_edu`.`course` ;
+
+CREATE TABLE IF NOT EXISTS `fmc_edu`.`course` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `timetable_id` INT NOT NULL,
+  `day` INT NOT NULL,
+  `course1` VARCHAR(45) NULL,
+  `course2` VARCHAR(45) NULL,
+  `course3` VARCHAR(45) NULL,
+  `course4` VARCHAR(45) NULL,
+  `course5` VARCHAR(45) NULL,
+  `course6` VARCHAR(45) NULL,
+  `course7` VARCHAR(45) NULL,
+  `course8` VARCHAR(45) NULL,
+  `course9` VARCHAR(45) NULL,
+  `course10` VARCHAR(45) NULL,
+  `course11` VARCHAR(45) NULL,
+  `course12` VARCHAR(45) NULL,
+  `available` TINYINT(1) NULL DEFAULT 1,
+  `last_update_date` DATETIME NULL,
+  PRIMARY KEY (`id`))
+ENGINE = MyISAM;
+
+
+-- -----------------------------------------------------
+-- Table `fmc_edu`.`image`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `fmc_edu`.`image` ;
+
+CREATE TABLE IF NOT EXISTS `fmc_edu`.`image` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `news_id` INT NOT NULL,
+  `img_name` VARCHAR(80) NOT NULL,
+  `img_path` VARCHAR(100) NOT NULL,
+  `creation_date` DATETIME NULL,
+  `last_update_date` DATETIME NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = MyISAM;
+
+
+-- -----------------------------------------------------
+-- Table `fmc_edu`.`slide`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `fmc_edu`.`slide` ;
+
+CREATE TABLE IF NOT EXISTS `fmc_edu`.`slide` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `subject` VARCHAR(60) NULL,
+  `news_id` INT NOT NULL,
+  `profile_id` INT NOT NULL,
+  `img_name` VARCHAR(80) NOT NULL,
+  `img_path` VARCHAR(100) NOT NULL,
+  `order` INT NOT NULL DEFAULT 0,
+  `start_date` DATETIME NULL,
+  `end_date` DATETIME NULL,
+  `available` TINYINT(1) NOT NULL DEFAULT 1,
+  `creation_date` DATETIME NULL,
+  `last_update_date` DATETIME NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = MyISAM;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
