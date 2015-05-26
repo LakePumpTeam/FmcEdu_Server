3b623ec0d843e13cbd5ede88f448e6e35859d7aa
Yove <billlllyove@hotmail.com>
2015-05-26 21:24:29 +0800

    Commit at 2015-05-26 21:23
    Fixed the reflection error cannot find getter/setter for property

--- src/main/java/com/fmc/edu/model/news/Comments.java
+++ src/main/java/com/fmc/edu/model/news/Comments.java
@@ -2,13 +2,20 @@ package com.fmc.edu.model.news;
 
 import com.fmc.edu.model.BaseBean;
 
+import java.sql.Timestamp;
+
 /**
  * Created by Yu on 2015/5/21.
  */
 public class Comments extends BaseBean {
+
     private int mNewsId;
+
     private int mProfileId;
+
     private String mComment;
+
+    private Timestamp mCreationDate;
 
     public int getNewsId() {
         return mNewsId;
@@ -32,5 +39,13 @@ public class Comments extends BaseBean {
 
     public void setComment(String pComment) {
         mComment = pComment;
+    }
+
+    public Timestamp getCreationDate() {
+        return mCreationDate;
+    }
+
+    public void setCreationDate(final Timestamp pCreationDate) {
+        mCreationDate = pCreationDate;
     }
 }
