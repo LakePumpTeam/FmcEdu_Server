a62d4570a964662fd56b0ec69051b59a355871b3
helloyuwei <753036576@qq.com>
2015-06-12 21:43:53 +0800

    Commit at 2015-05-16 03:56

--- src/main/java/com/fmc/edu/util/ValidationUtils.java
+++ src/main/java/com/fmc/edu/util/ValidationUtils.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
  */
 public class ValidationUtils {
 
-    protected static final String REGEX_PATTERN_PHONE_NUMBER = "^(((13[0-9])|(15([0-3]|[5-9]))|(18[0,5-9]))\\d{8})|(0\\d{2}-\\d{8})|(0\\d{3}-\\d{7})$";
+    protected static final String REGEX_PATTERN_PHONE_NUMBER = "^(((13[0-9])|(15([0-9]))|(17([0-9]))|(18[0-9]))\\d{8})|(0\\d{2}-\\d{8})|(0\\d{3}-\\d{7})$";
 
     protected static final Pattern PHONE_PATTERN = Pattern.compile(REGEX_PATTERN_PHONE_NUMBER);
 
