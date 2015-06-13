db49f69fc390a497252437ff7d3fdafad68ef647
Yove <billlllyove@hotmail.com>
2015-06-14 02:58:42 +0800

    Commit at 2015-6-14 02:58
    Update image utils

--- src/main/java/com/fmc/edu/configuration/WebConfig.java
+++ src/main/java/com/fmc/edu/configuration/WebConfig.java
@@ -13,139 +13,140 @@ import java.util.Properties;
  * Created by YW on 2015/5/3.
  */
 public class WebConfig {
-    private static final Logger LOG = Logger.getLogger(WebConfig.class);
+	private static final Logger LOG = Logger.getLogger(WebConfig.class);
 
-    public static final int DEPLOY_STATUS_DEVELOPER = 1;
-    public static final int DEPLOY_STATUS_PRODUCTION = 2;
-    private static final String DEPLOY_STATUS = "deployStatus";
-    private static final String ENCODE_BASE64_INPUT_PARAM = "encodeBase64InputParam";
+	public static final int DEPLOY_STATUS_DEVELOPER = 1;
+	public static final int DEPLOY_STATUS_PRODUCTION = 2;
+	private static final String DEPLOY_STATUS = "deployStatus";
+	private static final String ENCODE_BASE64_INPUT_PARAM = "encodeBase64InputParam";
 
-    private static final String API_KEY = "apiKey";
+	private static final String API_KEY = "apiKey";
 
-    private static final String SECRET_KEY = "secretKey";
+	private static final String SECRET_KEY = "secretKey";
 
-    private static final String DES_PASSWORD = "desPassword";
+	private static final String DES_PASSWORD = "desPassword";
 
-    private static final String DEFAULT_DES_PASSWORD = "9288028820109132570743325311898426347855298773549268758875018579537756572163084478873699447306034466200616411960574122434059469100235892702736860872901247123456";
+	private static final String DEFAULT_DES_PASSWORD = "9288028820109132570743325311898426347855298773549268758875018579537756572163084478873699447306034466200616411960574122434059469100235892702736860872901247123456";
 
-    private static final String IDENTITY_CODE_SURVIVOR_PERIOD = "identifyCodeSurvivalPeriod";
+	private static final String IDENTITY_CODE_SURVIVOR_PERIOD = "identifyCodeSurvivalPeriod";
 
-    private static final String UPLOAD_FILE_ROOT_PATH = "uploadFileRootPath";
+	private static final String UPLOAD_FILE_ROOT_PATH = "uploadFileRootPath";
 
-    private static final String COMPRESS_IMAGE_HIGHT = "compressedImageHight";
+	private static final String COMPRESS_IMAGE_HEIGHT = "compressedImageHight";
 
-    private static final String COMPRESS_IMAGE_WIDTH = "compressedImageWidth";
+	private static final String COMPRESS_IMAGE_WIDTH = "compressedImageWidth";
 
-    private static final String FMC_WEB_CONTEXT = "webContext";
-    private static Properties props;
+	private static final String FMC_WEB_CONTEXT = "webContext";
+	private static Properties props;
 
-    static {
-        Resource resource = new ClassPathResource("/web.properties");
-        try {
-            props = PropertiesLoaderUtils.loadProperties(resource);
-        } catch (IOException e) {
-            LOG.error("Loading web.properties failed.", e);
-        }
-    }
+	static {
+		Resource resource = new ClassPathResource("/web.properties");
+		try {
+			props = PropertiesLoaderUtils.loadProperties(resource);
+		} catch (IOException e) {
+			LOG.error("Loading web.properties failed.", e);
+		}
+	}
 
-    public static int deployStatus() {
+	public static int deployStatus() {
 
-        if (props == null) {
-            LOG.debug("web.properties read failed, then returning #WebConfig.DEPLOY_STATUS_DEVELOPER");
-            return WebConfig.DEPLOY_STATUS_DEVELOPER;
-        }
-        String deployStatus = props.getProperty(WebConfig.DEPLOY_STATUS);
-        if (StringUtils.isAlpha(deployStatus)) {
-            LOG.debug("\"deployStatus\" not was configured in the web.properties file, then returning #WebConfig.DEPLOY_STATUS_DEVEPLOPER");
-            return WebConfig.DEPLOY_STATUS_DEVELOPER;
-        }
-        return Integer.valueOf(deployStatus);
-    }
+		if (props == null) {
+			LOG.debug("web.properties read failed, then returning #WebConfig.DEPLOY_STATUS_DEVELOPER");
+			return WebConfig.DEPLOY_STATUS_DEVELOPER;
+		}
+		String deployStatus = props.getProperty(WebConfig.DEPLOY_STATUS);
+		if (StringUtils.isAlpha(deployStatus)) {
+			LOG.debug("\"deployStatus\" not was configured in the web.properties file, then returning #WebConfig.DEPLOY_STATUS_DEVEPLOPER");
+			return WebConfig.DEPLOY_STATUS_DEVELOPER;
+		}
+		return Integer.valueOf(deployStatus);
+	}
 
-    public static boolean isDevelopment() {
-        return deployStatus() == WebConfig.DEPLOY_STATUS_DEVELOPER;
-    }
+	public static boolean isDevelopment() {
+		return deployStatus() == WebConfig.DEPLOY_STATUS_DEVELOPER;
+	}
 
-    public static boolean isProduction() {
-        return !isDevelopment();
-    }
+	public static boolean isProduction() {
+		return !isDevelopment();
+	}
 
-    public static String getApiKey() throws Exception {
-        if (props == null) {
-            throw new Exception("Bai Du apiKey not was configured.");
-        }
-        return props.getProperty(WebConfig.API_KEY);
-    }
+	public static String getApiKey() throws Exception {
+		if (props == null) {
+			throw new Exception("Bai Du apiKey not was configured.");
+		}
+		return props.getProperty(WebConfig.API_KEY);
+	}
 
-    public static String getSecretKey() throws Exception {
-        if (props == null) {
-            throw new Exception("Bai Du secretKey not was configured.");
-        }
-        return props.getProperty(WebConfig.SECRET_KEY);
-    }
+	public static String getSecretKey() throws Exception {
+		if (props == null) {
+			throw new Exception("Bai Du secretKey not was configured.");
+		}
+		return props.getProperty(WebConfig.SECRET_KEY);
+	}
 
-    public static String getDESPassword() throws Exception {
-        if (props == null) {
-            throw new Exception("DES password not was configured.");
-        }
-        String password = props.getProperty(WebConfig.DES_PASSWORD);
-        if (StringUtils.isBlank(password)) {
-            return DEFAULT_DES_PASSWORD;
-        }
-        return password;
-    }
+	public static String getDESPassword() throws Exception {
+		if (props == null) {
+			throw new Exception("DES password not was configured.");
+		}
+		String password = props.getProperty(WebConfig.DES_PASSWORD);
+		if (StringUtils.isBlank(password)) {
+			return DEFAULT_DES_PASSWORD;
+		}
+		return password;
+	}
 
-    public static boolean isEncodeBase64InputParam() {
-        if (props == null) {
-            return false;
-        }
-        String encodeBase64InputParam = props.getProperty(WebConfig.ENCODE_BASE64_INPUT_PARAM);
-        if (StringUtils.isBlank(encodeBase64InputParam)) {
-            return false;
-        }
-        return Boolean.valueOf(encodeBase64InputParam);
-    }
+	public static boolean isEncodeBase64InputParam() {
+		if (props == null) {
+			return false;
+		}
+		String encodeBase64InputParam = props.getProperty(WebConfig.ENCODE_BASE64_INPUT_PARAM);
+		if (StringUtils.isBlank(encodeBase64InputParam)) {
+			return false;
+		}
+		return Boolean.valueOf(encodeBase64InputParam);
+	}
 
-    public static int getIdentifyCodeSurvivalPeriod() {
-        if (props == null) {
-            return 60;
-        }
-        String identifyCodeSurvivalPeriod = props.getProperty(WebConfig.IDENTITY_CODE_SURVIVOR_PERIOD);
-        return Integer.valueOf(identifyCodeSurvivalPeriod);
-    }
+	public static int getIdentifyCodeSurvivalPeriod() {
+		if (props == null) {
+			return 60;
+		}
+		String identifyCodeSurvivalPeriod = props.getProperty(WebConfig.IDENTITY_CODE_SURVIVOR_PERIOD);
+		return Integer.valueOf(identifyCodeSurvivalPeriod);
+	}
 
-    public static String getUploadFileRootPath() {
-        if (props == null) {
-            return "/var/upload";
-        }
-        String uploadFileRootPath = props.getProperty(WebConfig.UPLOAD_FILE_ROOT_PATH);
-        return uploadFileRootPath;
-    }
+	public static String getUploadFileRootPath() {
+		if (props == null) {
+			return "/var/upload";
+		}
+		String uploadFileRootPath = props.getProperty(WebConfig.UPLOAD_FILE_ROOT_PATH);
+		return uploadFileRootPath;
+	}
 
-    public static int getCompressedImageHight() {
-        if (props == null) {
-            return 60;
-        }
-        String compressedImageHight = props.getProperty(WebConfig.COMPRESS_IMAGE_HIGHT);
-        return Integer.valueOf(compressedImageHight);
-    }
 
-    public static int getCompressedImageWidth() {
-        if (props == null) {
-            return 60;
-        }
-        String compressedImageWidth = props.getProperty(WebConfig.COMPRESS_IMAGE_WIDTH);
-        return Integer.valueOf(compressedImageWidth);
-    }
+	public static int getCompressedImageHeight() {
+		if (props == null) {
+			return 60;
+		}
+		String compressedImageHight = props.getProperty(WebConfig.COMPRESS_IMAGE_HEIGHT);
+		return Integer.valueOf(compressedImageHight);
+	}
 
-    public static String getFMCWebContext() {
-        if (props == null) {
-            return "/";
-        }
-        String webContext = props.getProperty(WebConfig.FMC_WEB_CONTEXT);
-        if (StringUtils.isBlank(webContext)) {
-            return "/";
-        }
-        return webContext;
-    }
+	public static int getCompressedImageWidth() {
+		if (props == null) {
+			return 60;
+		}
+		String compressedImageWidth = props.getProperty(WebConfig.COMPRESS_IMAGE_WIDTH);
+		return Integer.valueOf(compressedImageWidth);
+	}
+
+	public static String getFMCWebContext() {
+		if (props == null) {
+			return "/";
+		}
+		String webContext = props.getProperty(WebConfig.FMC_WEB_CONTEXT);
+		if (StringUtils.isBlank(webContext)) {
+			return "/";
+		}
+		return webContext;
+	}
 }
--- src/main/java/com/fmc/edu/util/ImageUtils.java
+++ src/main/java/com/fmc/edu/util/ImageUtils.java
@@ -16,90 +16,100 @@ import java.io.IOException;
  * Created by Yu on 5/22/2015.
  */
 public class ImageUtils {
-    private static final Logger LOG = Logger.getLogger(ImageUtils.class);
+	private static final Logger LOG = Logger.getLogger(ImageUtils.class);
 
-    public static boolean writeFileToDisk(MultipartFile pFile, String userId, String fileName) throws IOException {
-        String highImagePath = ImageUtils.getHighBaseImagePath(userId);
-        String lowImagePath = ImageUtils.getLowBaseImagePath(userId);
-        File highBaseDir = new File(highImagePath);
-        File lowBaseDir = new File(lowImagePath);
-        if (!highBaseDir.exists()) {
-            highBaseDir.mkdirs();
-        }
-        if (!lowBaseDir.exists()) {
-            lowBaseDir.mkdirs();
-        }
+	public static boolean writeFileToDisk(MultipartFile pFile, String userId, String fileName) throws IOException {
+		String highImagePath = ImageUtils.getHighBaseImagePath(userId);
+		String lowImagePath = ImageUtils.getLowBaseImagePath(userId);
+		File highBaseDir = new File(highImagePath);
+		File lowBaseDir = new File(lowImagePath);
+		if (!highBaseDir.exists()) {
+			highBaseDir.mkdirs();
+		}
+		if (!lowBaseDir.exists()) {
+			lowBaseDir.mkdirs();
+		}
 
-        //write the original image
-        File highFile = new File(getHighImagePath(userId, fileName));
-        LOG.debug("Writing image to:" + getHighImagePath(userId, fileName));
-        byte[] bytes = pFile.getBytes();
-        BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(highFile));
-        stream.write(bytes);
-        IOUtils.closeQuietly(stream);
+		//write the original image
+		File highFile = new File(getHighImagePath(userId, fileName));
+		LOG.debug("Writing image to:" + getHighImagePath(userId, fileName));
+		byte[] bytes = pFile.getBytes();
+		BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(highFile));
+		stream.write(bytes);
+		IOUtils.closeQuietly(stream);
 
-        LOG.debug("Writing image completed.");
-        //write the compressed image
-        File lowFile = new File(getLowImagePath(userId, fileName));
-        LOG.debug("Writing compressed image to:" + getLowImagePath(userId, fileName));
-        byte[] lowBytes = new ImgCompress(pFile.getBytes()).resize(WebConfig.getCompressedImageWidth(), WebConfig.getCompressedImageHight());
-        stream = new BufferedOutputStream(new FileOutputStream(lowFile));
-        stream.write(lowBytes);
-        IOUtils.closeQuietly(stream);
-        LOG.debug("Writing compressed image completed.");
-        return true;
-    }
+		LOG.debug("Writing image completed.");
+		//write the compressed image
+		File lowFile = new File(getLowImagePath(userId, fileName));
+		LOG.debug("Writing compressed image to:" + getLowImagePath(userId, fileName));
+		byte[] lowBytes = new ImgCompress(pFile.getBytes()).resize(WebConfig.getCompressedImageWidth(), WebConfig.getCompressedImageHeight());
+		stream = new BufferedOutputStream(new FileOutputStream(lowFile));
+		stream.write(lowBytes);
+		IOUtils.closeQuietly(stream);
+		LOG.debug("Writing compressed image completed.");
+		return true;
+	}
 
-    public static String getHighBaseImagePath(final String userId) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(WebConfig.getUploadFileRootPath())
-                .append(File.separator)
-                .append("high")
-                .append(File.separator)
-                .append(getRelativePath(userId));
-        return FilenameUtils.normalizeNoEndSeparator(stringBuilder.toString());
-    }
+	public static String getSlideImagePath(String pFileName) {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append(WebConfig.getUploadFileRootPath())
+				.append(File.separator)
+				.append("slide")
+				.append(File.separator)
+				.append(pFileName);
+		return FilenameUtils.normalizeNoEndSeparator(stringBuilder.toString());
+	}
 
-    public static String getLowBaseImagePath(final String userId) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(WebConfig.getUploadFileRootPath())
-                .append(File.separator)
-                .append("low")
-                .append(File.separator)
-                .append(getRelativePath(userId));
-        return FilenameUtils.normalizeNoEndSeparator(stringBuilder.toString());
-    }
+	public static String getHighBaseImagePath(final String userId) {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append(WebConfig.getUploadFileRootPath())
+				.append(File.separator)
+				.append("high")
+				.append(File.separator)
+				.append(getRelativePath(userId));
+		return FilenameUtils.normalizeNoEndSeparator(stringBuilder.toString());
+	}
 
-    public static String getHighImagePath(String userId, String fileName) {
-        StringBuilder path = new StringBuilder();
-        path.append(getHighBaseImagePath(userId))
-                .append(File.separator)
-                .append(fileName);
-        return FilenameUtils.normalizeNoEndSeparator(path.toString());
-    }
+	public static String getLowBaseImagePath(final String userId) {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append(WebConfig.getUploadFileRootPath())
+				.append(File.separator)
+				.append("low")
+				.append(File.separator)
+				.append(getRelativePath(userId));
+		return FilenameUtils.normalizeNoEndSeparator(stringBuilder.toString());
+	}
 
-    public static String getLowImagePath(String userId, String fileName) {
-        StringBuilder path = new StringBuilder();
-        path.append(getLowBaseImagePath(userId))
-                .append(File.separator)
-                .append(fileName);
-        return FilenameUtils.normalizeNoEndSeparator(path.toString());
-    }
+	public static String getHighImagePath(String userId, String fileName) {
+		StringBuilder path = new StringBuilder();
+		path.append(getHighBaseImagePath(userId))
+				.append(File.separator)
+				.append(fileName);
+		return FilenameUtils.normalizeNoEndSeparator(path.toString());
+	}
 
-    public static String getRelativePath(String userId) {
-        StringBuilder relativePath = new StringBuilder();
-        relativePath.append(userId).append(File.separator);
-        return relativePath.toString();
-    }
+	public static String getLowImagePath(String userId, String fileName) {
+		StringBuilder path = new StringBuilder();
+		path.append(getLowBaseImagePath(userId))
+				.append(File.separator)
+				.append(fileName);
+		return FilenameUtils.normalizeNoEndSeparator(path.toString());
+	}
 
-    public static String getSuffixFromFileName(String pFileName) {
-        if (StringUtils.isBlank(pFileName)) {
-            return "";
-        }
-        int lastDot = pFileName.lastIndexOf(".");
-        if (lastDot == -1) {
-            return "";
-        }
-        return pFileName.substring(lastDot);
-    }
+	public static String getRelativePath(String userId) {
+		StringBuilder relativePath = new StringBuilder();
+		relativePath.append(userId).append(File.separator);
+		return relativePath.toString();
+	}
+
+	public static String getSuffixFromFileName(String pFileName) {
+		if (StringUtils.isBlank(pFileName)) {
+			return "";
+		}
+		int lastDot = pFileName.lastIndexOf(".");
+		if (lastDot == -1) {
+			return "";
+		}
+		return pFileName.substring(lastDot);
+	}
 }
